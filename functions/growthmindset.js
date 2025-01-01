0c50c1d36d95b7cc8cc82abd336196bd2d005aff
nsLittle <notsolittle88@gmail.com>
2025-01-01 15:40:43 -0500

    be?

---
+++ functions/growthmindset.js
@@ -0,0 +1,8 @@
+// functions/growthmindset.js
+exports.handler = async (event, context) => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Welcome to Growth Mindset!' }),
+    };
+  };
+  
