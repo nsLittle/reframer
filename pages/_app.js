ac9948ca1a87f1daca0bde15401cbf66ee0bcdb6
nsLittle <notsolittle88@gmail.com>
2025-01-09 05:51:26 -0500

    global.scss issues

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,21 @@
-import './globals.css'; 
+import '../globals.css'; // Adjust the path if needed
+
+import { Geist, Geist_Mono } from "next/font/google";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className={`${geistSans.variable} ${geistMono.variable}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
