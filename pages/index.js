2eda01efdd5e19dfa21e60d9fc54378bdd519cdd
nsLittle <notsolittle88@gmail.com>
2025-01-09 13:38:23 -0500

    firebaseConfig restored

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
 import Reframer from "../components/Reframer";
 import { initializeApp } from "firebase/app";
 
-// const firebaseConfig = {
-//   apiKey: "your-api-key",
-//   authDomain: "your-app.firebaseapp.com",
-//   projectId: "your-project-id",
-// };
+const firebaseConfig = {
+  apiKey: "your-api-key",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-project-id",
+};
 
 const app = initializeApp(firebaseConfig);
 
